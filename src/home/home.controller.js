1750e875d2efdce1bc86f263fff180a0b8f27741
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-04-06 11:39:47 -0600

    wess solution

--- src/home/home.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('app')
-.controller('HomeController', ['$scope', 'httpFactory', function($scope, httpFactory){
-  $scope.title = 'Pokemon!!';
-
-}]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,35 @@
-angular.module('app', ['ngRoute'])
-  .config(function($routeProvider){
-    $routeProvider
-      .when('/', {
-        templateUrl: 'home/home.html',
-        controller: 'HomeController'
-      })
-      .otherwise({redirectTo: '/'});
+angular.module("pokemonGenerator", [])
+.controller("MainController",
+            function ($http, $scope) {
+  $scope.generatePokemon = function () {
+    var random = Math.floor(Math.random() * 720)+1;
+    $scope.loadingName = true;
+
+    $http({
+      url: 'http://pokeapi.co/api/v2/pokemon/' +
+            random + '/',
+      method: 'GET'
+    })
+    .then(function (result) {
+      console.log(result.data.sprites);
+      $scope.pokemon = result.data;
+      $scope.img = result.data.sprites.front_default;
+      $scope.loadingName = false;
     });
+  };
+
+  $scope.generateMove = function () {
+    var random = Math.floor(Math.random() * 639)+1;
+    $scope.loadingMove = true;
+
+    $http({
+      url: 'http://pokeapi.co/api/v2/move/' +
+            random + '/',
+      method: 'GET'
+    })
+    .then(function (result) {
+      $scope.move = result.data.names[0].name;
+      $scope.loadingMove = false;
+    });
+  };
+});
